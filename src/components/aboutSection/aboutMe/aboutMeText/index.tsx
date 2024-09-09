fb483ef123a7f6074042daeb6187293a17845f5a
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-09 09:27:44 -0300

    added a imagem in aboutMeText component

--- src/components/aboutSection/aboutMe/aboutMeText/index.tsx
+++ src/components/aboutSection/aboutMe/aboutMeText/index.tsx
@@ -1,18 +1,24 @@
 import { fontSize, fontWeight, theme } from "@styles/theme";
 import styled from "styled-components";
 import { aboutMe } from "@json/index.json";
+import professionalImage from "@assets/imgs/professionalImage.jpg";
 
 export const AboutMeText = () => {
    return (
       <Container>
          <h2 className="title">Sobre mim</h2>
-         <div className="aboutText">
+         <p className="aboutText">
+            <img
+               src={professionalImage}
+               alt="Imagem profissional Dra. Luana"
+               className="aboutImage"
+            />
             {aboutMe.map((paragraph, index) => (
-               <p className="paragraph" key={index}>
+               <span key={index} className="paragraph">
                   {paragraph}
-               </p>
+               </span>
             ))}
-         </div>
+         </p>
       </Container>
    );
 };
@@ -29,14 +35,40 @@ const Container = styled.div`
    margin-bottom: 4rem;
 
    .aboutText {
-      display: flex;
-      flex-direction: column;
+      .aboutImage {
+         float: left;
+         height: auto;
+         min-height: 30rem;
+         max-height: 50rem;
+         object-fit: cover;
+         object-position: center;
+         border-radius: 0.5rem;
+         border: 0.2rem solid ${theme.primaryColor};
+         width: 30%;
+         max-width: 50rem;
+         margin: 0rem 2rem 1rem 0rem;
+      }
 
       .paragraph {
+         display: block;
          text-align: justify;
          color: ${theme.textColor};
          font-size: ${fontSize.basicSize};
          text-indent: 3rem;
+      }
+   }
+
+   @media (max-width: 768px) {
+      .aboutText {
+         display: flex;
+         flex-direction: column;
+         align-items: center;
+         gap: 2rem;
+
+         .aboutImage {
+            width: 100%;
+            margin: 0;
+         }
       }
    }
 `;
