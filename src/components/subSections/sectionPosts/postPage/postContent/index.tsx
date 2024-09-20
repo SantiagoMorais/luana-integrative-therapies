bed74b1035d1a13e481c2c0c543c8b0aee42b77f
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-20 11:17:27 -0300

    configured postContent style

--- src/components/subSections/sectionPosts/postPage/postContent/index.tsx
+++ src/components/subSections/sectionPosts/postPage/postContent/index.tsx
@@ -96,6 +96,10 @@ const Container = styled.div<{ $theme: ITheme }>`
          position: relative;
          font-size: ${fontSize.largeSize};
 
+         &::first-letter {
+            text-transform: capitalize;
+         }
+
          &::after {
             content: "";
             position: absolute;
@@ -113,17 +117,28 @@ const Container = styled.div<{ $theme: ITheme }>`
       }
 
       .descriptionContent {
-         .topicImage {
-            float: right;
-            height: 50dvh;
+         .topicImage,
+         img {
+            float: left;
             min-height: 30rem;
             max-height: 50rem;
             object-fit: cover;
             border-radius: 0.5rem;
-            border: 0.2rem solid ${({ $theme }) => $theme.primaryColor};
             width: 50%;
             max-width: 50rem;
-            margin: 0rem 0rem 1rem 2rem;
+            margin: 0rem 2rem 1rem 0rem;
+         }
+
+         .topicImage {
+            border: 0.2rem solid ${({ $theme }) => $theme.primaryColor};
+            height: 50dvh;
+            max-height: 50rem;
+         }
+
+         img {
+            border: 0.2rem solid ${({ $theme }) => $theme.tertiaryColor};
+            height: 40dvh;
+            max-height: 40rem;
          }
 
          .imageNotFound {
@@ -160,6 +175,16 @@ const Container = styled.div<{ $theme: ITheme }>`
             margin-bottom: 1rem;
             color: ${({ $theme }) => $theme.textColor};
 
+            a {
+               color: ${({ $theme }) => $theme.tertiaryColor};
+               font-weight: ${fontWeight.medium};
+               transition: 0.3s;
+
+               &:hover {
+                  opacity: 0.7;
+               }
+            }
+
             &:last-child {
                margin-bottom: 0;
             }
@@ -167,7 +192,9 @@ const Container = styled.div<{ $theme: ITheme }>`
 
          h2,
          h3,
-         h4 {
+         h4,
+         h5,
+         h6 {
             position: relative;
             margin: 2rem 0;
             color: ${({ $theme }) => $theme.textColor};
@@ -199,13 +226,27 @@ const Container = styled.div<{ $theme: ITheme }>`
             }
          }
 
-         h4 {
+         h4,
+         h5,
+         h6 {
             font-size: ${fontSize.mediumSize};
             font-weight: ${fontWeight.thin};
 
             strong {
                font-weight: ${fontWeight.medium};
             }
+         }
+
+         blockquote {
+            font-size: ${fontSize.smallSize};
+            border-left: 0.2rem solid rgba(0, 0, 0, 0.342);
+            display: inline-block;
+            color: rgba(0, 0, 0, 0.7);
+            font-weight: ${fontWeight.medium};
+            margin: 1rem 0;
+            background-color: rgba(85, 85, 85, 0.2);
+            border-radius: 0 1rem 1rem 0;
+            padding: 0.5rem 1rem;
          }
 
          ul,
@@ -242,6 +283,45 @@ const Container = styled.div<{ $theme: ITheme }>`
             li {
                list-style-type: decimal;
             }
+         }
+
+         table {
+            width: 100%;
+
+            thead {
+               tr {
+                  th {
+                     background-color: ${({ $theme }) => $theme.primaryColor};
+                     border: 0.1rem solid rgba(0, 0, 0, 0.6);
+
+                     p {
+                        text-align: center;
+                        text-indent: 0;
+                     }
+                  }
+               }
+            }
+
+            tbody {
+               tr {
+                  td {
+                     border: 0.1rem solid rgba(0, 0, 0, 0.6);
+                     p {
+                        text-align: center;
+                        text-indent: 0;
+                     }
+                  }
+               }
+            }
+         }
+
+         video {
+            display: block;
+            border-radius: 1rem;
+            padding: 2rem 0;
+            width: 100%;
+            height: calc(60dvw * 9 / 16);
+            max-height: calc(80rem * 9 / 16);
          }
       }
 
@@ -318,7 +398,8 @@ const Container = styled.div<{ $theme: ITheme }>`
    @media (max-width: 768px) {
       .postContent {
          .descriptionContent {
-            .topicImage {
+            .topicImage,
+            img {
                float: none;
                height: auto;
                min-height: auto;
